b4ec2996cc070ae87f4458b24e71923684f0a80e
Surya Vadivazhagu <22950119+svadivazhagu@users.noreply.github.com>
2020-09-29 16:03:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ client.on('message', msg => {
 	if (msg.content === '*sl') {
 		const author = msg.author['username'];
 		const date = new Date();
-		if (String((date.getMonth() + 1)) + date.getDate() === '1028') {
+		if (String((date.getMonth() + 1)) + date.getDate() === '1026') {
 			msg.channel.send(author + ', WoW Shadowlands out! Get on!');
 		}
 		else {
-			msg.channel.send(author + ", WoW Shadowlands is not out yet. It's coming on 10/29/2020.")
+			msg.channel.send(author + ", WoW Shadowlands is not out yet. It's coming on 10/26/2020.")
 		}
 
 		// console.log( + );
