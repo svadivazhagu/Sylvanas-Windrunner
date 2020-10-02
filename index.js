cbe69d62462c8aceb51d84fa3cddab72ba2daa5d
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2020-10-02 10:18:15 -0400

    updates given SL delay and prepatch date out.

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ client.once('ready', () => {
 client.on('message', msg => {
 	if (msg.content === '*sl' && msg.channel['name'] === 'ness-mains') {
 		const author = msg.author['username'];
-		const date = new Date();
-		if (String((date.getMonth() + 1)) + date.getDate() === '1026') {
-			msg.channel.send(author + ', WoW Shadowlands is out! Get on!');
-		}
-		else {
-			const time = date.getTime();
-			const shadowlands_time = Math.round((1603670400000 - time) / (60 * 60 * 24 * 1000)) + 1;
-			msg.channel.send(author + ", WoW Shadowlands is not out yet. It's coming on 10/26/2020. That's " + String(shadowlands_time) + ' days away. For the Horde!');
-		}
+		msg.channel.send(author + ", SL release date hasn't been announced yet. We don't know when it's coming out. Stay tuned!");
 	}
 	if (msg.content === '*prepatch' && msg.channel['name'] === 'ness-mains') {
 		const author = msg.author['username'];
-		msg.channel.send(author + ', WoW Shadowlands Pre-Patch date has not been announced yet.');
+		const date = new Date();
+		if (String((date.getMonth() + 1)) + date.getDate() === '1013') {
+			msg.channel.send(author + ', Pre-patch AKA SL waiting room is out. Get on if you want, I guess.');
+		}
+		else {
+			const time = date.getTime();
+			const prepatch_time = Math.round((1602561600000 - time) / (60 * 60 * 24 * 1000)) + 1;
+			msg.channel.send(author + ", WoW SL Pre-Patch is not out yet. It's coming on 10/13/2020. That's " + String(prepatch_time) + ' days away. For the Horde!');
+		}
 	}
 });
 
